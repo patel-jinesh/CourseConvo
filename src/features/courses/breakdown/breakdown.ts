2fbe850fa8131cfce280e4ce66f805ab065b7f8f
Jinesh Patel <programmer.jinesh@gmail.com>
2020-10-22 22:15:11 -0400

    add breakdown redux

--- src/features/courses/breakdown/breakdown.ts
+++ src/features/courses/breakdown/breakdown.ts
@@ -1,5 +1,6 @@
 import { Course } from "../course";
 import { User } from "../../user/user"
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 export interface Mark {
     type: string;
@@ -8,8 +9,50 @@ export interface Mark {
 }
 
 export interface Breakdown {
+    id: string,
     course: Course;
-    isAnonymous: boolean;
     user: User;
     marks: Mark[];
+    isAnonymous: boolean;
 }
+
+/**
+ * Redux Section
+ */
+export interface BreakdownsState {
+    reviews: { [id: string]: Breakdown },
+}
+
+export enum BreakdownActions {
+    ADD = "ADD",
+    EDIT = "EDIT",
+    DELETE = "DELETE"
+}
+
+const initialState: BreakdownsState = {
+    reviews: {},
+};
+
+const breakdownsRedux = createSlice({
+    name: "REVIEWS",
+    initialState,
+    reducers: {
+        add(state, action: PayloadAction<Breakdown>) {
+            state.reviews = { [action.payload.id]: action.payload, ...state.reviews }
+        },
+        edit(state, action: PayloadAction<Breakdown>) {
+            state.reviews[action.payload.id] = action.payload;
+        },
+        remove(state, action: PayloadAction<string>) {
+            delete state.reviews[action.payload];
+        }
+    }
+});
+
+export const {
+    add,
+    edit,
+    remove
+} = breakdownsRedux.actions;
+
+export default breakdownsRedux.reducer;
