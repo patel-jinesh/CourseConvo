b0c1e12bc5ce234ae18004b51ec5aae53464604e
Jinesh Patel <programmer.jinesh@gmail.com>
2020-11-05 23:47:39 -0500

    added other menu items

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import { Layout, Menu } from 'antd';
 import {
   AuditOutlined,
   HomeOutlined,
+  PieChartOutlined,
+  CommentOutlined,
+  InfoCircleOutlined
 } from '@ant-design/icons';
 import { Link, Route, Switch, BrowserRouter as Router, useLocation } from 'react-router-dom';
 import HomePage from './pages/HomePage';
@@ -12,17 +15,29 @@ import HomePage from './pages/HomePage';
 const { Sider } = Layout;
 const { SubMenu } = Menu;
 
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
 function Nav() {
   let location = useLocation();
+  let query = useQuery();
 
   return <Sider>
     <Menu theme="dark" mode="inline" defaultSelectedKeys={[location.pathname]}>
-      <Menu.Item key="/" icon={<HomeOutlined />}>
-        <Link to="/">Home</Link>
-      </Menu.Item>
-      <Menu.Item key="/academics" icon={<AuditOutlined />}>
-        <Link to="/academics">Academic Records</Link>
-      </Menu.Item>
+      <Menu.ItemGroup title="Main">
+        <Menu.Item key="/" icon={<HomeOutlined />}>
+          <Link to="/">Home</Link>
+        </Menu.Item>
+        <Menu.Item key="/academics" icon={<AuditOutlined />}>
+          <Link to="/academics">Academic Records</Link>
+        </Menu.Item>
+      </Menu.ItemGroup>
+      <Menu.ItemGroup title="Courses">
+        <Menu.Item key="/information" icon={<InfoCircleOutlined />}>Information</Menu.Item>
+        <Menu.Item key="/breakdown" icon={<PieChartOutlined/>}>Breakdown</Menu.Item>
+        <Menu.Item key="/reviews" icon={<CommentOutlined />}>Reviews</Menu.Item>
+      </Menu.ItemGroup>
     </Menu>
   </Sider>
 }
