2c3a1ed443ec69eb2eb6e13bebeb0bc4eeaa0d63
Mohammed Mirajkar <mirajkam@mcmaster.ca>
2020-11-11 13:11:39 -0500

    added code for top popular courses

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -58,16 +58,18 @@ class HomePage extends React.Component<Props, State> {
     }
 
     getMostPopularCourses = (numCourse: number) => {
-        let mostPopularCourses: any = {};
+        let mostPopularCourses: { [courseID: string]: { count: number } } = {};
         for (let i = 0; i < this.props.reviews.length; i++) {
             let review = this.props.reviews[i];
             mostPopularCourses = {
                 ...mostPopularCourses, [this.props.instances[review.instanceID].courseID]: {
-                    count: (mostPopularCourses[this.props.instances[review.instanceID].courseID]?.count ?? 0) + 1
+                    count: (mostPopularCourses[this.props.instances[review.instanceID].courseID]?.count ?? 0) + 1,
                 }
             };
         }
 
+        let courseReviews = Object.entries(mostPopularCourses).map(([courseID, { count }]) => ({ count: count, courseID: courseID }));
+        return courseReviews.sort((a, b) => b.count - a.count).slice(0, numCourse);
     }
 
 
@@ -120,36 +122,14 @@ class HomePage extends React.Component<Props, State> {
 
                     <Col span={8} >
                         <Card>
-                            <List header="Elective Course Recommendation"
+                            <List header="Most Popular Courses"
                                 itemLayout="horizontal"
-                                dataSource={this.getTopRatedCourses(3)}
+                                dataSource={this.getMostPopularCourses(3)}
                                 renderItem={item => (
                                     <List.Item>
                                         <Row style={{ width: "100%" }}>
                                             <Col flex={1}>{`${this.props.courses[item.courseID].subject} ${this.props.courses[item.courseID].code}`}</Col>
-                                            <Col>
-                                                <Statistic
-                                                    value={item.rating}
-                                                    precision={2}
-                                                    suffix="/ 5"
-
-                                                    valueStyle={{
-                                                        color: (() => {
-                                                            if (item.rating < 1) {
-                                                                return '#f5222d'
-                                                            } else if (item.rating < 2) {
-                                                                return '#874d00'
-                                                            } else if (item.rating < 3) {
-                                                                return '#fadb14'
-                                                            } else if (item.rating < 4) {
-                                                                return '#7cb305'
-                                                            } else if (item.rating < 5) {
-                                                                return '#52c41a'
-                                                            }
-                                                        })()
-                                                    }}
-                                                />
-                                            </Col>
+                                            <Col>{`${item.count} reviews`}</Col>
                                         </Row>
 
                                     </List.Item>
