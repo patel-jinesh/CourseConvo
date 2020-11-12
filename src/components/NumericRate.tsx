4a300023e808f7668a53f79ddc01a39e08b5001a
Archit Shah <shaha18@mcmaster.ca>
2020-11-12 11:07:14 -0500

    added custom rates

---
+++ src/components/NumericRate.tsx
@@ -0,0 +1,51 @@
+import { RootState } from "../app/store";
+import { connect, ConnectedProps } from "react-redux";
+import React from "react";
+import { Rate } from "antd";
+
+import { FrownOutlined, MehOutlined, SmileOutlined } from '@ant-design/icons';
+
+type ComponentProps = {
+    tooltips?: string[]
+    defaultValue?: number
+    disabled?: boolean
+    onChange?: (value : number) => void
+}
+
+type ComponentState = {}
+
+const mapState = (state: RootState, props: ComponentProps) => ({
+});
+
+const mapDispatch = {
+}
+
+const connector = connect(mapState, mapDispatch);
+
+type ReduxProps = ConnectedProps<typeof connector>;
+type Props = ReduxProps & ComponentProps;
+
+const customCharacters = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+];
+
+class NumericRate extends React.Component<Props> {
+    render() {
+        return (
+            <Rate
+                tooltips={this.props.tooltips}
+                character={({ index, value }: { index: number, value: number }) => {
+                    return customCharacters[index];
+                }}
+                defaultValue={this.props.defaultValue}
+                disabled={this.props.disabled}
+                onChange={this.props.onChange}/>
+        );
+    }
+}
+
+export default connector(NumericRate);
---
+++ src/components/SmileRate.tsx
@@ -0,0 +1,58 @@
+import { RootState } from "../app/store";
+import { connect, ConnectedProps } from "react-redux";
+import React from "react";
+import { Rate } from "antd";
+
+import { FrownOutlined, MehOutlined, SmileOutlined } from '@ant-design/icons';
+
+type ComponentProps = {
+    tooltips?: string[]
+    defaultValue?: number
+    disabled?: boolean
+}
+
+type ComponentState = {}
+
+const mapState = (state: RootState, props: ComponentProps) => ({
+});
+
+const mapDispatch = {
+}
+
+const connector = connect(mapState, mapDispatch);
+
+type ReduxProps = ConnectedProps<typeof connector>;
+type Props = ReduxProps & ComponentProps;
+
+const customIconsColored = [
+    <FrownOutlined style={{ color: 'red' }} />,
+    <FrownOutlined style={{ color: 'orange' }} />,
+    <MehOutlined style={{ color: 'yellow' }} />,
+    <SmileOutlined style={{ color: 'greenyellow' }} />,
+    <SmileOutlined style={{ color: 'green' }} />,
+];
+
+const customIconsBlank = [
+    <FrownOutlined style={{ color: 'rgba(255,255,255,0.12)' }} />,
+    <FrownOutlined style={{ color: 'rgba(255,255,255,0.12)' }} />,
+    <MehOutlined style={{ color: 'rgba(255,255,255,0.12)' }} />,
+    <SmileOutlined style={{ color: 'rgba(255,255,255,0.12)' }} />,
+    <SmileOutlined style={{ color: 'rgba(255,255,255,0.12)' }} />,
+]
+
+class SmileRate extends React.Component<Props> {
+    render() {
+        return (
+            <Rate
+                tooltips={this.props.tooltips}
+                character={({ index, value }: { index: number, value: number }) => {
+                    if (index === value - 1)
+                        return customIconsColored[index];
+                    return customIconsBlank[index];
+                }}
+                defaultValue={this.props.defaultValue} disabled={this.props.disabled} />
+        );
+    }
+}
+
+export default connector(SmileRate);
