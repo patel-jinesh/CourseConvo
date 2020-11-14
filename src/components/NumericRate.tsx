5d7f51d72a5fea005d3e520daf5022ce5d57e5cf
Jinesh Patel <programmer.jinesh@gmail.com>
2020-11-14 14:59:43 -0500

    changed numericrate to radio

--- src/components/NumericRate.tsx
+++ src/components/NumericRate.tsx
@@ -1,11 +1,10 @@
-import { Rate } from "antd";
+import { Rate, Radio } from "antd";
 import React from "react";
 import { connect, ConnectedProps } from "react-redux";
 import { RootState } from "../app/store";
 
 
 type ComponentProps = {
-    tooltips?: string[]
     defaultValue?: number
     disabled?: boolean
     onChange?: (value : number) => void
@@ -24,25 +23,20 @@ const connector = connect(mapState, mapDispatch);
 type ReduxProps = ConnectedProps<typeof connector>;
 type Props = ReduxProps & ComponentProps;
 
-const customCharacters = [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-];
-
 class NumericRate extends React.Component<Props> {
     render() {
         return (
-            <Rate
-                tooltips={this.props.tooltips}
-                character={({ index, value }: { index: number, value: number }) => {
-                    return customCharacters[index];
-                }}
+            <Radio.Group
+                buttonStyle='solid'
                 defaultValue={this.props.defaultValue}
                 disabled={this.props.disabled}
-                onChange={this.props.onChange}/>
+                onChange={e => this.props.onChange && this.props.onChange(e.target.value)}>
+                <Radio.Button value={1}>1</Radio.Button>
+                <Radio.Button value={2}>2</Radio.Button>
+                <Radio.Button value={3}>3</Radio.Button>
+                <Radio.Button value={4}>4</Radio.Button>
+                <Radio.Button value={5}>5</Radio.Button>
+            </Radio.Group>
         );
     }
 }
